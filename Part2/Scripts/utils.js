595673f17c92e36e1579054a64616421397ad97c
Markovic <markoviclaki03.4@gmail.com>
2025-01-12 22:39:11 +0100

    I did it?

--- Part2/Scripts/utils.js
+++
@@ -1,9 +0,0 @@
-//JS
-let previousWidth = window.innerWidth;
-window.addEventListener('resize',() => {
-    const currentWidth = window.innerWidth;
-    if(previousWidth<=820 && currentWidth > 820){
-        window.scrollTo(0,0);
-    }
-    previousWidth=currentWidth;
-});
