353523509868495d613cb422acbc927645c718ec
unknown <stefanpetkovic.mes@gmail.com>
2025-01-12 04:24:57 +0100

    +Fixed Card animations +started making json info

--- Part2/Scripts/utils.js
+++ Part2/Scripts/utils.js
@@ -2,7 +2,6 @@
 let previousWidth = window.innerWidth;
 window.addEventListener('resize',() => {
     const currentWidth = window.innerWidth;
-    console.log(currentWidth);
     if(previousWidth<=820 && currentWidth > 820){
         window.scrollTo(0,0);
     }
